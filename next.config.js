332f080845fdad5bd9958168bb95cd994fd8e7a2
Olivierkepler <olivierkfrancois1@gmail.com>
2024-11-28 02:02:58 -0500

    Your commit message here

--- next.config.js
+++ next.config.js
@@ -1,14 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
   images: {
-    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
         hostname: '**',
       },
     ],
+    domains: ['images.unsplash.com', 'your-other-domain.com'],
   },
 }
 
